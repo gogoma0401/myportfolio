a7133b5307ec1bacfd2a92d1b0c768b34f45e760
gogoma <gogoma0401@naver.com>
2024-04-19 03:10:11 +0900

    수정

--- project/kfc/index.js
+++ project/kfc/index.js
@@ -1,20 +1,52 @@
 $(document).ready(function () {
-    $(document).ready(function () {
-        $('.sub_menu').hide();
-        $('.main_menu').on('mouseenter', function () {
-            $('.sub_menu').stop().fadeIn();
-        });
-        $('.main_menu').on('mouseleave', function () {
-            $('.sub_menu').stop().fadeOut();
-        });
-        $('.sub_menu>li').on('mouseenter', function () {
-            $(this).css('background-color', '#fe9f2ba4');
-        });
-        $('.sub_menu>li').on('mouseleave', function () {
-            $(this).css('background-color', '');
-        });
+    $('.sub_menu').hide();
+    $('.main_menu').on('mouseenter', function () {
+        $('.sub_menu').stop().fadeIn();
+    });
+    $('.main_menu').on('mouseleave', function () {
+        $('.sub_menu').stop().fadeOut();
+    });
+    $('.sub_menu>li').on('mouseenter', function () {
+        $(this).css('background-color', '#fe9f2ba4');
+    });
+    $('.sub_menu>li').on('mouseleave', function () {
+        $(this).css('background-color', '');
     });
     // 이미지 배열
+    let swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 4,
+        breakpoints: {
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 2,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 2,
+            },
+            450: {
+                slidesPerView: 2,
+                spaceBetween: 1,
+            },
+        },
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 2,
+            slideShadows: false,
+        },
+        initialSlide: 1,
+        observer: true,
+        observeParents: true,
+    });
 
     var images = document.querySelectorAll('.load_imgbox img');
     var check = 0;
@@ -30,6 +62,18 @@ $(document).ready(function () {
     }
     play();
 
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.from(".info", {
+        scrollTrigger: {
+            trigger: ".info",
+            start: "top center",
+            end: "bottom center",
+            scrub: 2,
+        },
+        rotation: -360,
+        x: -700,
+        duration: 1,
+    });
 
     // 모바일
     $('.app_menu .fa-bars').click(function () {
@@ -47,9 +91,8 @@ $(document).ready(function () {
     document.querySelector('.box').addEventListener('mouseenter', function () {
         this.classList.add('show-scrollbar');
     });
-
     document.querySelector('.box').addEventListener('mouseleave', function () {
         this.classList.remove('show-scrollbar');
     });
-    
+
 });
